309634af1a981e80a51cdf8d445c0cd908ab2fd3
skt-t1-byungi <tiniwz@gmail.com>
2018-01-29 08:35:16 +0900

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')({pattern: ["*"]});
+const $ = require('gulp-load-plugins')({pattern: ['*']});
 const path = require('path');
 const pkg = require('./package.json');
 
@@ -17,24 +17,26 @@ const paths = {
   DIST_IMAGE: path.join(pkg.paths.DIST, pkg.paths.DIR_IMAGE)
 };
 
-const onError = function(err) {
+const onError = function (err) {
   console.log('An Error Has Occurred \n', err.toString());
   this.emit('end');
 };
 
 let shouldNewer = true;
 
-gulp.task('edge', ()=>{
+gulp.task('edge', () => {
   return gulp
     .src('./src/edge/**/!(_*).edge')
     .pipe($.plumber({errorHandler: onError}))
     .pipe($.if(shouldNewer, $.newer({dest: paths.DEV_HTML, ext: '.html'})))
-    .pipe($.edgejs())
+    .pipe($.edgejs({
+      assets_img: '/assets/image/'
+    }))
     .pipe(gulp.dest(paths.DEV_HTML))
     .pipe($.browserSync.stream());
 });
 
-gulp.task('scss', ()=>{
+gulp.task('scss', () => {
   return gulp
     .src('./src/scss/**/!(_*).scss')
     .pipe($.plumber({errorHandler: onError}))
@@ -46,7 +48,7 @@ gulp.task('scss', ()=>{
     .pipe($.browserSync.stream());
 });
 
-gulp.task('js', ()=>{
+gulp.task('js', () => {
   return gulp
     .src('./src/script/**/!(_*).js')
     .pipe($.plumber({errorHandler: onError}))
@@ -56,26 +58,26 @@ gulp.task('js', ()=>{
     .pipe($.browserSync.stream());
 });
 
-gulp.task('image', ()=>{
+gulp.task('image', () => {
   return gulp
     .src('./src/image/**/*.{png,jpg,gif,jpeg,svg}')
     .pipe($.imagemin())
     .pipe(gulp.dest(paths.DEV_IMAGE));
 });
 
-gulp.task('sprites:del-prev-files', ()=>{
+gulp.task('sprites:del-prev-files', () => {
   return $.del('./src/sprites/**/*.{css,scss}');
 });
 
-gulp.task('sprites', ['sprites:del-prev-files'], ()=>{
+gulp.task('sprites', ['sprites:del-prev-files'], () => {
   const taskStreams = [];
 
-  paths.sprites.forEach(sprite=>{
+  paths.sprites.forEach(sprite => {
     const stream = gulp
       .src(sprite.src)
       .pipe($.spritesmith({
         imgName: sprite.imgName,
-        cssName: sprite.cssName,
+        cssName: sprite.cssName
       }));
 
     const imgStream = stream.img
@@ -86,13 +88,13 @@ gulp.task('sprites', ['sprites:del-prev-files'], ()=>{
     const cssStream = stream.css
       .pipe(gulp.dest('./src/sprites'));
 
-      taskStreams.push(imgStream, cssStream);
+    taskStreams.push(imgStream, cssStream);
   });
 
   return $.mergeStream(...taskStreams);
 });
 
-gulp.task('bower:js', ()=>{
+gulp.task('bower:js', () => {
   return gulp
     .src('./bower.json')
     .pipe($.mainBowerFiles('**/*.js'))
@@ -100,7 +102,7 @@ gulp.task('bower:js', ()=>{
     .pipe(gulp.dest(paths.DEV_SCRIPT));
 });
 
-gulp.task('bower:css', ()=>{
+gulp.task('bower:css', () => {
   return gulp
     .src('./bower.json')
     .pipe($.mainBowerFiles('**/*.css'))
@@ -110,59 +112,59 @@ gulp.task('bower:css', ()=>{
 
 gulp.task('bower', ['bower:js', 'bower:css']);
 
-gulp.task('watch', ['sprites', 'bower', 'edge', 'scss', 'js'], ()=>{
-  gulp.watch("src/**/!(_*).edge", ['edge']);
-  gulp.watch("src/**/!(_*).scss", ['scss']);
-  gulp.watch("src/**/!(_*).js", ['js']);
+gulp.task('watch', ['sprites', 'bower', 'edge', 'scss', 'js'], () => {
+  gulp.watch('src/**/!(_*).edge', ['edge']);
+  gulp.watch('src/**/!(_*).scss', ['scss']);
+  gulp.watch('src/**/!(_*).js', ['js']);
 
-  const doWithoutNewer = taskName => ()=>{
+  const doWithoutNewer = taskName => () => {
     shouldNewer = false;
-    return $.runSequence(taskName, ()=>{
+    return $.runSequence(taskName, () => {
       shouldNewer = true;
     });
   };
 
-  gulp.watch("src/**/_*.edge", doWithoutNewer('edge'));
-  gulp.watch("src/**/_*.scss", doWithoutNewer('scss'));
-  gulp.watch("src/**/_*.js", doWithoutNewer('js'));
+  gulp.watch('src/**/_*.edge', doWithoutNewer('edge'));
+  gulp.watch('src/**/_*.scss', doWithoutNewer('scss'));
+  gulp.watch('src/**/_*.js', doWithoutNewer('js'));
 });
 
-gulp.task('serve', ()=>{
+gulp.task('serve', () => {
   $.browserSync.init({server: paths.DEV, port: 8081});
   return $.runSequence('watch');
 });
 
-gulp.task('serve-r', ()=>{
+gulp.task('serve-r', () => {
   $.browserSync.init({server: paths.DEV, port: 8081, open: false});
   return $.runSequence('watch');
 });
 
-gulp.task('dev:del', ()=>{
+gulp.task('dev:del', () => {
   return $.del(path.join(paths.DEV, '**/*'));
 });
 
-gulp.task('dev', ()=>{
+gulp.task('dev', () => {
   return $.runSequence('dev:del', ['bower', 'edge', 'scss', 'js']);
 });
 
-gulp.task('build:js', ['bower', 'js'], ()=>{
+gulp.task('build:js', ['bower', 'js'], () => {
   return gulp
     .src(path.join(paths.DEV_SCRIPT, '**/*.js'))
     .pipe($.uglify())
     .pipe($.size({showFiles: true}))
-    .pipe(gulp.dest(paths.DIST_SCRIPT))
+    .pipe(gulp.dest(paths.DIST_SCRIPT));
 });
 
-gulp.task('build:css', ['scss'], ()=>{
+gulp.task('build:css', ['scss'], () => {
   return gulp
     .src(path.join(paths.DEV_CSS, '**/*.css'))
     .pipe($.autoprefixer())
     .pipe($.cleanCss())
     .pipe($.size({showFiles: true}))
-    .pipe(gulp.dest(paths.DIST_CSS))
+    .pipe(gulp.dest(paths.DIST_CSS));
 });
 
-gulp.task('build:html', ['edge'], ()=>{
+gulp.task('build:html', ['edge'], () => {
   return gulp
     .src(path.join(paths.DEV_HTML, '**/*.html'))
     .pipe($.jsbeautifier())
@@ -170,25 +172,25 @@ gulp.task('build:html', ['edge'], ()=>{
     .pipe(gulp.dest(paths.DIST_HTML));
 });
 
-gulp.task('build:image', ['image'], ()=>{
+gulp.task('build:image', ['image'], () => {
   return gulp
     .src(path.join(paths.DEV_IMAGE, '**/*.{png,jpg,gif,jpeg,svg}'))
     .pipe($.size({showFiles: true}))
     .pipe(gulp.dest(paths.DIST_IMAGE));
 });
 
-gulp.task('build:del', ()=>{
+gulp.task('build:del', () => {
   return $.del(path.join(paths.DIST, '**/*'));
 });
 
-gulp.task('build', ()=>{
+gulp.task('build', () => {
   shouldNewer = false;
-  return $.runSequence('build:del', ['build:html', 'build:css', 'build:js', 'build:image'], ()=>shouldNewer = true);
+  return $.runSequence('build:del', ['build:html', 'build:css', 'build:js', 'build:image'], () => shouldNewer = true);
 });
 
 gulp.task('clean', ['sprites:del-prev-files', 'dev:del', 'build:del']);
 
-gulp.task('bump', ()=>{
+gulp.task('bump', () => {
   return gulp
     .src(['./bower.json', './package.json'])
     .pipe($.bump())
@@ -197,6 +199,5 @@ gulp.task('bump', ()=>{
 
 gulp.task('w3c', function () {
   gulp.src('./dist/*.html')
-    .pipe($.w3cjs())
-    .pipe($.w3cjs.reporter());
+    .pipe($.w3cjs());
 });
---
+++ src/script/_custom-select.js
@@ -0,0 +1,113 @@
+(function () {
+  $.fn.customSelect = function (option) {
+    option = $.extend({
+      // defaults
+      prefix: 'custom-',
+      selectedClass: 'selected',
+      unselectedClass: '',
+      visibleTextClass: 'visible',
+      invisibleTextClass: '',
+      activeDropdownClass: 'active',
+      inactiveDropdownClass: '',
+      onChange: $.noop
+    }, option);
+
+    this.each(function () {
+      var $customSelect = $(this);
+      var $customTexts = $customSelect.find('[data-' + option.prefix + 'text]');
+      var $dropdown = $customSelect.find('[data-' + option.prefix + 'options]');
+      var $customOptions = $dropdown.find('[data-' + option.prefix + 'value]');
+
+      // default values
+      var name = $customSelect.data(option.prefix + 'name');
+      var values = $customOptions
+        .map(function () {
+          return $(this).data(option.prefix + 'value');
+        })
+        .toArray();
+
+      // state
+      var isActiveDropdown = false;
+      var selectedValue = $customOptions.filter('[data-' + option.prefix + 'selected]').val() || values[0];
+
+      /**
+       * $(..).val(), $(..).serialize() 할 경우 값을 추가하기 위한 용도의 select element
+       */
+      var $select = createSelectEl(name, values)
+        .appendTo($customSelect).val(selectedValue);
+
+      // events
+      $customSelect.click(function (e) {
+        if ($dropdown.has(e.target).length === 0) {
+          isActiveDropdown = !isActiveDropdown;
+          renderDropdown();
+        }
+      });
+
+      $customOptions.click(function () {
+        var $selected = $(this);
+        var value = $selected.data(option.prefix + 'value');
+
+        if (isActiveDropdown) {
+          isActiveDropdown = false;
+          renderDropdown();
+        }
+
+        if (value === selectedValue) {
+          return;
+        }
+
+        selectedValue = value;
+        $select.val(selectedValue).change();
+        option.onChange.call($customSelect[0], selectedValue);
+
+        renderText();
+      });
+
+      $(document).click(function (e) {
+        if ($customSelect.has(e.target).length === 0 && isActiveDropdown) {
+          isActiveDropdown = false;
+          renderDropdown();
+        }
+      });
+
+      // init render
+      renderText();
+      renderDropdown();
+
+      function renderText () {
+        var $selected = $customTexts.filter('[data-' + option.prefix + 'text=' + selectedValue + ']');
+        var $others = $customTexts.not($selected);
+
+        $selected.addClass(option.visibleTextClass).removeClass(option.invisibleTextClass);
+        $others.addClass(option.invisibleTextClass).removeClass(option.visibleTextClass);
+      }
+
+      function renderDropdown () {
+        // disabled
+        if (!isActiveDropdown) {
+          $dropdown.addClass(option.inactiveDropdownClass).removeClass(option.activeDropdownClass);
+          return;
+        }
+
+        // enabled
+        var $selected = $customTexts.filter('[data-' + option.prefix + 'text=' + selectedValue + ']');
+        var $others = $customTexts.not($selected);
+
+        $selected.addClass(option.selectedClass).removeClass(option.unselectedClass);
+        $others.addClass(option.unselectedClass).removeClass(option.selectedClass);
+
+        $dropdown.addClass(option.activeDropdownClass).removeClass(option.inactiveDropdownClass);
+      }
+    });
+  };
+
+  function createSelectEl (name, values) {
+    return $('<select></select>', {name: name})
+      .html(
+        values.map(function (value) {
+          return '<option value="' + value + '">' + value + '</option>';
+        })
+      ).hide();
+  }
+})();
